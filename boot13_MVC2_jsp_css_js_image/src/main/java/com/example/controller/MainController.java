f6155ba2248821a9c89098bd5920f630775e3b99
user <7834jsb@gmail.com>
2024-04-05 11:04:02 +0900

    jenkins야 지금빌드 자동으로 해줘야한다~

--- boot13_MVC2_jsp_css_js_image/src/main/java/com/example/controller/MainController.java
+++ boot13_MVC2_jsp_css_js_image/src/main/java/com/example/controller/MainController.java
@@ -9,6 +9,7 @@ public class MainController {
 	@GetMapping("/")
 	public String main() {
 		System.out.println("/ 주소 요청///////");
+		System.out.println("/ jenkins에서 자동적인 배포함");
 		return "main";
 	}
 	@GetMapping("/loginForm")
